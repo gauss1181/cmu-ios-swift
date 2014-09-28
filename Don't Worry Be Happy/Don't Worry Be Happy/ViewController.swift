bf69d501b25c2f98377f7c7149ae9a0baaa702b7
Megan Chen <mcgauss1181@gmail.com>
2014-09-28 04:22:52 -0400

    Starter code

---
+++ Don't Worry Be Happy/Don't Worry Be Happy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Don't Worry Be Happy
+//
+//  Created by Megan Chen on 9/28/14.
+//  Copyright (c) 2014 Megan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
